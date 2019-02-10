99c0f6b6381fe0c25a0ff346a794adb5ba694572
taehong kim <7538518@naver.com>
2019-02-10 23:02:37 +0900

    2019.02.10 fizzbuzz
    *수정*
    fizzbuzz로 수정(for문)

---
+++ tutorial/src/exam02/FizzBuzz.kt
@@ -0,0 +1,22 @@
+package exam02
+
+fun fizzBuzz(i: Int) = when {
+    i % 15 == 0 -> "FizzBuzz"
+    i % 3 == 0 -> "Fizz"
+    i % 5 == 0 -> "Buzz"
+    else -> "${i}"
+}
+fun main(args: Array<String>){
+//    for (i in 1..100) {
+//        println(fizzBuzz(i))
+//    }
+//    for (i in 100 downTo 1) {
+//        println(fizzBuzz(i))
+//    }
+    for (i in 100 downTo 1 step 2) {
+        println(fizzBuzz(i))
+    }
+    for (i in 1 until 10){
+        println(i)
+    }
+}
